7aad2d5ff91f2ef3f64fd9afdb477a1a2ab5579d
yugogamer <57806524+yugogamer@users.noreply.github.com>
2021-11-18 23:16:54 +0100

    [fix] change fonction name to rust standard : authUser -> auth_user

--- src/controlleur/authentification.rs
+++ src/controlleur/authentification.rs
@@ -1,12 +1,12 @@
 use crate::entity::authentification::AuthResponse;
 
 pub fn load_road(loader : rocket::Rocket<rocket::Build>) -> rocket::Rocket<rocket::Build> {
-    return loader.mount("/client/cle/verification", routes![authUser]);
+    return loader.mount("/client/cle/verification", routes![auth_user]);
 }
 
 
 #[get("/<id>")]
-async fn authUser(id: &str) -> String {
+async fn auth_user(id: &str) -> String {
     let mut response = AuthResponse {
         status: String::from(""),
         request: String::from(""),
