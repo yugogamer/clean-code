01b0e51081f8ce6cbc40e171f18aafab0211fd16
yugogamer <57806524+yugogamer@users.noreply.github.com>
2021-11-19 01:04:13 +0100

    [func] implementing authentification service in authentification controller

--- src/controlleur/authentification.rs
+++ src/controlleur/authentification.rs
@@ -1,4 +1,5 @@
 use crate::entity::authentification::AuthResponse;
+use crate::service::authentification::generate_id;
 
 pub fn load_road(loader : rocket::Rocket<rocket::Build>) -> rocket::Rocket<rocket::Build> {
     return loader.mount("/client/cle/verification", routes![auth_user]);
@@ -13,7 +14,19 @@ async fn auth_user(id: &str) -> String {
         result: false,
     };
 
-    
+    let key = generate_id(id);
+
+    match key{
+        Ok(key) => {
+            response.request = String::from(key);
+            response.status = String::from("Succes");
+            response.result = true;
+        },
+        Err(err) => {
+            response.request = err.to_string();
+            response.status = String::from("Errors");
+        }
+    }
 
     return serde_json::to_string(&response).unwrap();
 }
