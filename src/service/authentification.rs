c6d550bef8438092a4bcd959a57467ccd497796a
dgoncalv <goncalves.denisstmg2@gmail.com>
2021-12-06 14:16:39 +0100

    make_sum debug, WIP

--- src/service/authentification.rs
+++ src/service/authentification.rs
@@ -92,7 +92,7 @@ pub fn generate_id(key: &str) -> Result<String, anyhow::Error>{
 }
 
 
-fn make_sum(char_array: &[char]) -> u32{
+fn make_sum(char_array: &Vec<char>) -> u32{
     let mut sum : u32 = 0;
     for number in char_array{
         if number.is_numeric(){
@@ -152,8 +152,8 @@ mod test{
     #[test_case("2222222222","20".to_string())]
     #[test_case("123456789","45".to_string())]
     fn test_make_sum(entry: &str, expected: String){
-        let char_array = entry.chars().collect();
-        let result = make_sum(entry);
+        let char_array: Vec<char> = entry.chars().collect();
+        let result = make_sum(&char_array);
         assert_eq!(result.to_string(), expected);
     }
     
